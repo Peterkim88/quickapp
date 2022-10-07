558f6e5ab7c872f5335b39afd68356aa9bbff958
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-10-07 05:43:43 -0400

    updated header and footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ function CustomerHeader() {
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav className="ms-auto">
 
               <LinkContainer to="/cart">
                 <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>

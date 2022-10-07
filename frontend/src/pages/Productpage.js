317d680758e6acf8a095b5014075c5114e493a0b
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-10-07 02:45:34 -0400

    created product details actions and reducers

--- frontend/src/pages/Productpage.js
+++ frontend/src/pages/Productpage.js
@@ -1,90 +1,94 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProductDetails } from "../actions/productDetailsActions";
 
 function ProductPage() {
   const params = useParams();
-  // const product = products.find((p) => p._id == params.id)
-  const [product, setProduct] = useState([])
+  const productDetails = useSelector(state => state.productDetails)
+  const {loading, error, product} = productDetails
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
-
-    async function fetchProduct(){
-      const { data } = await axios.get(`/api/products/${params.id}`)
-      setProduct(data)
-    }
-
-    fetchProduct()
-    
-  }, [])
+    dispatch(listProductDetails(params.id))
+  }, [dispatch])
   
   return (
     <div>
       <Link to='/' className="btn btn-light my-3">Go Back</Link>
-      <Row>
+      {loading ?
+        <Loader />
+        : error 
+        ? <Message variant='danger'>{error}</Message>
+        : (
+          <Row>
 
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
 
-        <Col md={3}>
-          <ListGroup variant="flush">
+            <Col md={3}>
+              <ListGroup variant="flush">
 
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              Price: ${product.price}
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  Price: ${product.price}
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              Description: {product.description}
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  Description: {product.description}
+                </ListGroup.Item>
 
-          </ListGroup>
-        </Col>
+              </ListGroup>
+            </Col>
 
-        <Col md={3}>
-          <Card>
+            <Col md={3}>
+              <Card>
 
-            <ListGroup variant="flush">
+                <ListGroup variant="flush">
 
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-              <ListGroup.Item>
-                <Button className="btn btn-primary w-100" disabled={product.countInStock <= 0} type="button">Add to Cart</Button>
-              </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button className="btn btn-primary w-100" disabled={product.countInStock <= 0} type="button">Add to Cart</Button>
+                  </ListGroup.Item>
 
-            </ListGroup>
-            
-          </Card>
-        </Col>
+                </ListGroup>
+                
+              </Card>
+            </Col>
 
-      </Row>
+          </Row>
+        )
+      }
     </div>
   )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import { productDetailsReducer } from './reducers/productDetailsReducers';
 import { productListReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
-  productList: productListReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer
 });
 
 export const initialState = {};

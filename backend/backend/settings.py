296af1449e63d6058cc3666ef0bb34141daa5cbb
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-11-02 14:29:05 -0400

    check bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'quick-mj97.onrender.com',

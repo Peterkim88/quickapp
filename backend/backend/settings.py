f8ab2e966cde2e306539232c497cbd8de1c372a4
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-11-03 00:19:08 -0400

    testing deploy again

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,10 +23,10 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'quick-mj97.onrender.com/',
+    'quick-mj97.onrender.com',
     '127.0.0.1', 
     'localhost'
 ]
